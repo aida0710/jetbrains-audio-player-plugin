05626307c1a587454eb661f19e554d5ef0037cdc
Masaki Aida <51500566+aida0710@users.noreply.github.com>
2026-03-10 15:13:14 +0900

    fix: IDEが音声ファイル拡張子を認識するようAudioFileTypeを登録

---
+++ src/main/kotlin/com/github/audioplayer/AudioFileType.kt
@@ -0,0 +1,28 @@
+package com.github.audioplayer
+
+import com.intellij.openapi.fileTypes.FileType
+import javax.swing.Icon
+
+class AudioFileType private constructor() : FileType {
+
+    override fun getName(): String = "Audio"
+
+    override fun getDescription(): String = "Audio file"
+
+    override fun getDefaultExtension(): String = "mp3"
+
+    override fun getIcon(): Icon? = null
+
+    override fun isBinary(): Boolean = true
+
+    override fun isReadOnly(): Boolean = true
+
+    companion object {
+        val INSTANCE = AudioFileType()
+
+        val EXTENSIONS = listOf(
+            "mp3", "wav", "ogg", "flac", "aac", "m4a", "wma",
+            "opus", "ape", "aiff", "aif"
+        )
+    }
+}
